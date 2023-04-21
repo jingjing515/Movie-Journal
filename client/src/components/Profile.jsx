64e06b7310b9bd8529ab45d92ad8bb73672d3398
jingjing515 <majingjing515@outlook.com>
2023-04-21 02:17:14 -0700

    add edit name button for profile

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,18 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useState } from "react";
 
 export default function Profile() {
   const { user } = useAuth0();
+  const [name, setName] = useState(user.name);
+
+  const handleNameChange = () => {
+    const newName = window.prompt("Enter your new user name", name);
+    if (newName) {
+      setName(newName);
+    }
+  };
 
   return (
     <div>
       <div>
-        <p>Name: {user.name}</p>
+        <p>Name: {name}</p>
       </div>
+
       <div>
         <img src={user.picture} width="70" alt="profile avatar" />
       </div>
       <div>
         <p>📧 Email: {user.email}</p>
+        <button onClick={handleNameChange}>Edit Name</button>
       </div>
 
       {/* <div>
